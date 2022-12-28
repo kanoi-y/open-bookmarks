409386d76dac860e92a78a9adf0c8b531a49ad1b
kanoi-y <furafuratonbo@gmail.com>
2022-12-28 21:17:11 +0900

    🔨 manifest.jsonの設定を記述

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Open Bookmarks",
+  version: "1.0.0",
+  permissions: ["bookmarks"],
+  action: {
+    default_popup: "index.html",
+  },
+  commands: {
+    _execute_action: {
+      suggested_key: {
+        default: "Ctrl+Shift+Y",
+      },
+    },
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), crx({ manifest })],
 })
